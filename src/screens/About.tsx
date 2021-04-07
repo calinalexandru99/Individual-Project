8215a8a1ea54e5a96de1e31ae838c065357b0bd5
calinalexandru99 <47211078+calinalexandru99@users.noreply.github.com>
2021-04-07 15:01:58 +0300

    Add navigation bar to all screens

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import TopBar from "../components/TopBar";
 
 const About: React.FC = () => {
     return (<>
+        <TopBar showBack>
+
+        </TopBar>
     </>)
 }
 export default About;
--- src/screens/Credits.tsx
+++ src/screens/Credits.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import TopBar from "../components/TopBar";
 
 const Credits: React.FC = () => {
     return (<>
+        <TopBar showBack>
+
+        </TopBar>
     </>)
 }
 export default Credits;
